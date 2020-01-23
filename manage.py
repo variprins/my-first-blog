8da774629362f84506afd86e06068660d14e9ea4
roxana <variprins@hotmail.com>
2020-01-23 05:19:28 -0500

    agregadas vistas.

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
@@ -13,7 +12,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
